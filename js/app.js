00283a30998635ffd71d496a9c231136bf3676ec
Pavel Maksimovich <maksimovich.pvl@gmail.com>
2017-03-23 14:15:00 +0200

    x

---
+++ js/app.js
@@ -0,0 +1,324 @@
+var tableData = {
+    items: [
+        {"id":1,"name":"Feednation","price":142,"quantity":299},
+        {"id":2,"name":"Shuffledrive","price":840,"quantity":551},
+        {"id":3,"name":"Skyba","price":922,"quantity":218},
+        {"id":4,"name":"Skinder","price":820,"quantity":430},
+        {"id":5,"name":"Bubblebox","price":593,"quantity":686},
+        {"id":6,"name":"Youbridge","price":423,"quantity":686},
+        {"id":7,"name":"Centimia","price":922,"quantity":990},
+        {"id":8,"name":"Gabspot","price":650,"quantity":958},
+        {"id":9,"name":"Voonder","price":105,"quantity":860},
+        {"id":10,"name":"Oyoyo","price":470,"quantity":82},
+        {"id":11,"name":"Centimia","price":299,"quantity":264},
+        {"id":12,"name":"Skinder","price":861,"quantity":239},
+        {"id":13,"name":"Realblab","price":635,"quantity":53},
+        {"id":14,"name":"Bubblemix","price":120,"quantity":990},
+        {"id":15,"name":"Eidel","price":895,"quantity":270},
+        {"id":16,"name":"Twitterworks","price":844,"quantity":843},
+        {"id":17,"name":"Edgeclub","price":783,"quantity":302},
+        {"id":18,"name":"Eazzy","price":203,"quantity":753},
+        {"id":19,"name":"Zoomlounge","price":950,"quantity":49},
+        {"id":20,"name":"Flashset","price":404,"quantity":296},
+        {"id":21,"name":"Skimia","price":632,"quantity":474},
+        {"id":22,"name":"LiveZ","price":881,"quantity":662},
+        {"id":23,"name":"Riffpath","price":229,"quantity":411},
+        {"id":24,"name":"Agimba","price":479,"quantity":611},
+        {"id":25,"name":"Abata","price":922,"quantity":250},
+        {"id":26,"name":"Skipstorm","price":170,"quantity":999},
+        {"id":27,"name":"Meevee","price":181,"quantity":134},
+        {"id":28,"name":"Meevee","price":904,"quantity":850},
+        {"id":29,"name":"Skyble","price":51,"quantity":201},
+        {"id":30,"name":"Gigashots","price":761,"quantity":920},
+        {"id":31,"name":"Yakidoo","price":614,"quantity":818},
+        {"id":32,"name":"Mydo","price":152,"quantity":327},
+        {"id":33,"name":"Myworks","price":133,"quantity":629},
+        {"id":34,"name":"Voonte","price":806,"quantity":651},
+        {"id":35,"name":"Livetube","price":272,"quantity":529},
+        {"id":36,"name":"Jamia","price":932,"quantity":208},
+        {"id":37,"name":"Yabox","price":483,"quantity":736},
+        {"id":38,"name":"Meedoo","price":37,"quantity":83},
+        {"id":39,"name":"Bubbletube","price":422,"quantity":864},
+        {"id":40,"name":"Mita","price":467,"quantity":21},
+        {"id":41,"name":"Cogidoo","price":700,"quantity":107},
+        {"id":42,"name":"Photojam","price":514,"quantity":553},
+        {"id":43,"name":"Yodel","price":637,"quantity":499},
+        {"id":44,"name":"DabZ","price":220,"quantity":913},
+        {"id":45,"name":"Feedmix","price":736,"quantity":565},
+        {"id":46,"name":"Demizz","price":125,"quantity":644},
+        {"id":47,"name":"Avaveo","price":866,"quantity":12},
+        {"id":48,"name":"Meejo","price":694,"quantity":715},
+        {"id":49,"name":"Fatz","price":670,"quantity":165},
+        {"id":50,"name":"Buzzdog","price":763,"quantity":517},
+        {"id":51,"name":"Tagtune","price":379,"quantity":601},
+        {"id":52,"name":"Twitterlist","price":172,"quantity":613},
+        {"id":53,"name":"Rhynyx","price":76,"quantity":764},
+        {"id":54,"name":"Minyx","price":319,"quantity":117},
+        {"id":55,"name":"Skinte","price":765,"quantity":958},
+        {"id":56,"name":"Meezzy","price":2,"quantity":641},
+        {"id":57,"name":"Feedfish","price":261,"quantity":987},
+        {"id":58,"name":"Quire","price":117,"quantity":21},
+        {"id":59,"name":"Feedfish","price":81,"quantity":602},
+        {"id":60,"name":"JumpXS","price":866,"quantity":992},
+        {"id":61,"name":"Edgepulse","price":565,"quantity":680},
+        {"id":62,"name":"Meetz","price":257,"quantity":707},
+        {"id":63,"name":"Thoughtsphere","price":651,"quantity":400},
+        {"id":64,"name":"Zoonder","price":424,"quantity":436},
+        {"id":65,"name":"Youspan","price":381,"quantity":837},
+        {"id":66,"name":"Divanoodle","price":882,"quantity":868},
+        {"id":67,"name":"Digitube","price":347,"quantity":909},
+        {"id":68,"name":"Demimbu","price":871,"quantity":333},
+        {"id":69,"name":"Quamba","price":672,"quantity":61},
+        {"id":70,"name":"Chatterpoint","price":247,"quantity":946},
+        {"id":71,"name":"Bubbletube","price":95,"quantity":203},
+        {"id":72,"name":"Agimba","price":385,"quantity":237},
+        {"id":73,"name":"Avavee","price":128,"quantity":916},
+        {"id":74,"name":"Youopia","price":529,"quantity":568},
+        {"id":75,"name":"Mycat","price":533,"quantity":21},
+        {"id":76,"name":"Chatterbridge","price":806,"quantity":516},
+        {"id":77,"name":"Flashspan","price":589,"quantity":552},
+        {"id":78,"name":"Podcat","price":507,"quantity":551},
+        {"id":79,"name":"Jabbertype","price":62,"quantity":336},
+        {"id":80,"name":"Wordtune","price":277,"quantity":243},
+        {"id":81,"name":"Devshare","price":177,"quantity":22},
+        {"id":82,"name":"Innotype","price":307,"quantity":173},
+        {"id":83,"name":"Photobean","price":791,"quantity":220},
+        {"id":84,"name":"Photospace","price":960,"quantity":454},
+        {"id":85,"name":"Latz","price":640,"quantity":587},
+        {"id":86,"name":"Blognation","price":242,"quantity":862},
+        {"id":87,"name":"Photolist","price":911,"quantity":629},
+        {"id":88,"name":"Katz","price":860,"quantity":740},
+        {"id":89,"name":"Kazu","price":265,"quantity":334},
+        {"id":90,"name":"Gabtype","price":937,"quantity":962},
+        {"id":91,"name":"Minyx","price":482,"quantity":667},
+        {"id":92,"name":"Miboo","price":57,"quantity":478},
+        {"id":93,"name":"Gevee","price":164,"quantity":207},
+        {"id":94,"name":"Mycat","price":542,"quantity":61},
+        {"id":95,"name":"Gabcube","price":817,"quantity":867},
+        {"id":96,"name":"Skilith","price":399,"quantity":130},
+        {"id":97,"name":"Thoughtworks","price":632,"quantity":771},
+        {"id":98,"name":"Livetube","price":780,"quantity":452},
+        {"id":99,"name":"Oyonder","price":183,"quantity":348},
+        {"id":100,"name":"Topicblab","price":82,"quantity":784},
+        {"id":101,"name":"Flipbug","price":33,"quantity":173},
+        {"id":102,"name":"Avavee","price":268,"quantity":635},
+        {"id":103,"name":"Rooxo","price":947,"quantity":340},
+        {"id":104,"name":"Blogpad","price":72,"quantity":341},
+        {"id":105,"name":"Oozz","price":237,"quantity":380},
+        {"id":106,"name":"Dynava","price":270,"quantity":22},
+        {"id":107,"name":"Browsezoom","price":85,"quantity":77},
+        {"id":108,"name":"Vinte","price":28,"quantity":551},
+        {"id":109,"name":"Leexo","price":532,"quantity":870},
+        {"id":110,"name":"Thoughtstorm","price":368,"quantity":933},
+        {"id":111,"name":"Rhyzio","price":783,"quantity":452},
+        {"id":112,"name":"Topicshots","price":128,"quantity":597},
+        {"id":113,"name":"Zoomzone","price":336,"quantity":206},
+        {"id":114,"name":"Trupe","price":58,"quantity":485},
+        {"id":115,"name":"Jayo","price":936,"quantity":82},
+        {"id":116,"name":"Jabbersphere","price":365,"quantity":367},
+        {"id":117,"name":"Cogilith","price":994,"quantity":268},
+        {"id":118,"name":"Zoonoodle","price":447,"quantity":933},
+        {"id":119,"name":"Skinix","price":832,"quantity":927},
+        {"id":120,"name":"Eare","price":852,"quantity":103},
+        {"id":121,"name":"Babbleblab","price":313,"quantity":247},
+        {"id":122,"name":"Realpoint","price":908,"quantity":445},
+        {"id":123,"name":"Realfire","price":423,"quantity":852},
+        {"id":124,"name":"Voonyx","price":835,"quantity":541},
+        {"id":125,"name":"Youbridge","price":837,"quantity":884},
+        {"id":126,"name":"Eidel","price":545,"quantity":473},
+        {"id":127,"name":"Kazio","price":559,"quantity":254},
+        {"id":128,"name":"Riffpedia","price":526,"quantity":865},
+        {"id":129,"name":"Jetpulse","price":764,"quantity":951},
+        {"id":130,"name":"Mita","price":420,"quantity":267},
+        {"id":131,"name":"Youtags","price":203,"quantity":708},
+        {"id":132,"name":"Wordpedia","price":112,"quantity":43},
+        {"id":133,"name":"Rhyzio","price":361,"quantity":392},
+        {"id":134,"name":"Centizu","price":533,"quantity":530},
+        {"id":135,"name":"Trilith","price":166,"quantity":496},
+        {"id":136,"name":"Eadel","price":459,"quantity":701},
+        {"id":137,"name":"Tazzy","price":453,"quantity":714},
+        {"id":138,"name":"Centizu","price":954,"quantity":791},
+        {"id":139,"name":"Photobug","price":641,"quantity":975},
+        {"id":140,"name":"Livetube","price":255,"quantity":227},
+        {"id":141,"name":"Zoonoodle","price":183,"quantity":442},
+        {"id":142,"name":"Abata","price":328,"quantity":103},
+        {"id":143,"name":"Twinder","price":819,"quantity":623},
+        {"id":144,"name":"Jaxnation","price":215,"quantity":302},
+        {"id":145,"name":"Devcast","price":711,"quantity":294},
+        {"id":146,"name":"Dynazzy","price":723,"quantity":34},
+        {"id":147,"name":"Midel","price":285,"quantity":166},
+        {"id":148,"name":"Linktype","price":304,"quantity":198},
+        {"id":149,"name":"Bluejam","price":866,"quantity":807},
+        {"id":150,"name":"Zoozzy","price":36,"quantity":794},
+        {"id":151,"name":"Topicstorm","price":350,"quantity":487},
+        {"id":152,"name":"Pixonyx","price":229,"quantity":398},
+        {"id":153,"name":"Topiczoom","price":567,"quantity":212},
+        {"id":154,"name":"Topdrive","price":439,"quantity":4},
+        {"id":155,"name":"Yakitri","price":507,"quantity":719},
+        {"id":156,"name":"Wordtune","price":689,"quantity":362},
+        {"id":157,"name":"Dynazzy","price":235,"quantity":250},
+        {"id":158,"name":"Wordware","price":258,"quantity":251},
+        {"id":159,"name":"Reallinks","price":289,"quantity":471},
+        {"id":160,"name":"Talane","price":665,"quantity":19},
+        {"id":161,"name":"Twitterlist","price":761,"quantity":695},
+        {"id":162,"name":"Edgeify","price":323,"quantity":979},
+        {"id":163,"name":"Browseblab","price":43,"quantity":588},
+        {"id":164,"name":"Mynte","price":445,"quantity":640},
+        {"id":165,"name":"Buzzbean","price":451,"quantity":84},
+        {"id":166,"name":"Realpoint","price":836,"quantity":954},
+        {"id":167,"name":"Innotype","price":490,"quantity":873},
+        {"id":168,"name":"Pixope","price":208,"quantity":175},
+        {"id":169,"name":"Skaboo","price":667,"quantity":356},
+        {"id":170,"name":"Photobug","price":912,"quantity":219},
+        {"id":171,"name":"Kayveo","price":620,"quantity":220},
+        {"id":172,"name":"Twinte","price":245,"quantity":649},
+        {"id":173,"name":"Gigashots","price":316,"quantity":693},
+        {"id":174,"name":"Ntags","price":699,"quantity":819},
+        {"id":175,"name":"Realpoint","price":128,"quantity":265},
+        {"id":176,"name":"Youopia","price":168,"quantity":502},
+        {"id":177,"name":"Twitternation","price":790,"quantity":839},
+        {"id":178,"name":"Dynabox","price":896,"quantity":946},
+        {"id":179,"name":"Livetube","price":968,"quantity":779},
+        {"id":180,"name":"Realpoint","price":552,"quantity":131},
+        {"id":181,"name":"Tavu","price":295,"quantity":986},
+        {"id":182,"name":"Mynte","price":929,"quantity":862},
+        {"id":183,"name":"Kare","price":495,"quantity":764},
+        {"id":184,"name":"Kwilith","price":253,"quantity":855},
+        {"id":185,"name":"Jabbercube","price":139,"quantity":890},
+        {"id":186,"name":"Tagchat","price":999,"quantity":899},
+        {"id":187,"name":"Topiczoom","price":92,"quantity":97},
+        {"id":188,"name":"Twitterbridge","price":23,"quantity":393},
+        {"id":189,"name":"Brightbean","price":807,"quantity":612},
+        {"id":190,"name":"Fiveclub","price":341,"quantity":56},
+        {"id":191,"name":"Realmix","price":426,"quantity":465},
+        {"id":192,"name":"Thoughtstorm","price":756,"quantity":568},
+        {"id":193,"name":"Rhyzio","price":110,"quantity":613},
+        {"id":194,"name":"BlogXS","price":918,"quantity":487},
+        {"id":195,"name":"Browsezoom","price":396,"quantity":280},
+        {"id":196,"name":"Yabox","price":221,"quantity":299},
+        {"id":197,"name":"Yabox","price":324,"quantity":728},
+        {"id":198,"name":"Voonte","price":856,"quantity":19},
+        {"id":199,"name":"Tanoodle","price":325,"quantity":323},
+        {"id":200,"name":"Edgeclub","price":443,"quantity":742},
+        {"id":201,"name":"Demizz","price":996,"quantity":420},
+        {"id":202,"name":"Oyoloo","price":191,"quantity":971},
+        {"id":203,"name":"Jabberbean","price":880,"quantity":560},
+        {"id":204,"name":"Riffpedia","price":349,"quantity":337},
+        {"id":205,"name":"Blogpad","price":779,"quantity":736},
+        {"id":206,"name":"Cogidoo","price":290,"quantity":879},
+        {"id":207,"name":"Roombo","price":665,"quantity":585},
+        {"id":208,"name":"Quimm","price":978,"quantity":264},
+        {"id":209,"name":"Youspan","price":284,"quantity":294},
+        {"id":210,"name":"Twinder","price":512,"quantity":90},
+        {"id":211,"name":"Browsedrive","price":649,"quantity":988},
+        {"id":212,"name":"Tagopia","price":913,"quantity":717},
+        {"id":213,"name":"Avaveo","price":366,"quantity":620},
+        {"id":214,"name":"Fiveclub","price":899,"quantity":696},
+        {"id":215,"name":"Flashpoint","price":345,"quantity":444},
+        {"id":216,"name":"Feednation","price":943,"quantity":639},
+        {"id":217,"name":"Rhyloo","price":874,"quantity":442},
+        {"id":218,"name":"Skaboo","price":237,"quantity":648},
+        {"id":219,"name":"Fivechat","price":997,"quantity":750},
+        {"id":220,"name":"Trunyx","price":955,"quantity":99},
+        {"id":221,"name":"Snaptags","price":715,"quantity":541},
+        {"id":222,"name":"Yodo","price":332,"quantity":48},
+        {"id":223,"name":"Roodel","price":274,"quantity":410},
+        {"id":224,"name":"Dabshots","price":250,"quantity":663},
+        {"id":225,"name":"Twitternation","price":272,"quantity":556},
+        {"id":226,"name":"Yadel","price":797,"quantity":112},
+        {"id":227,"name":"Linklinks","price":349,"quantity":496},
+        {"id":228,"name":"Tambee","price":340,"quantity":403},
+        {"id":229,"name":"Jaxworks","price":1,"quantity":629},
+        {"id":230,"name":"Vipe","price":972,"quantity":732},
+        {"id":231,"name":"Jetwire","price":853,"quantity":695},
+        {"id":232,"name":"Flashset","price":624,"quantity":972},
+        {"id":233,"name":"Tagtune","price":574,"quantity":219},
+        {"id":234,"name":"Npath","price":726,"quantity":648},
+        {"id":235,"name":"Browsebug","price":550,"quantity":229},
+        {"id":236,"name":"Feedfish","price":655,"quantity":72},
+        {"id":237,"name":"Vipe","price":118,"quantity":476},
+        {"id":238,"name":"Eayo","price":879,"quantity":762},
+        {"id":239,"name":"Vinte","price":361,"quantity":179},
+        {"id":240,"name":"Oodoo","price":294,"quantity":771},
+        {"id":241,"name":"Skyba","price":202,"quantity":710},
+        {"id":242,"name":"Thoughtmix","price":347,"quantity":214},
+        {"id":243,"name":"Kwideo","price":54,"quantity":437},
+        {"id":244,"name":"Skyndu","price":686,"quantity":653},
+        {"id":245,"name":"Skyble","price":897,"quantity":314},
+        {"id":246,"name":"Meejo","price":630,"quantity":474},
+        {"id":247,"name":"Buzzster","price":633,"quantity":446},
+        {"id":248,"name":"Livetube","price":399,"quantity":437},
+        {"id":249,"name":"Centidel","price":818,"quantity":873},
+        {"id":250,"name":"Kimia","price":686,"quantity":888}
+    ]
+};
+
+
+
+
+
+
+
+var purchaseApp = angular.module("purchaseApp", []).filter('startFrom', function(){
+        return function(input, start){
+            start = +start;
+            return input.slice(start);
+        }
+    });
+
+    purchaseApp.controller("purchaseController", function ($scope) {
+    $scope.list = tableData;
+
+    // Сортировка по умолчинию
+    $scope.sortType     = 'id'; 
+    $scope.sortReverse  = false; 
+
+    $scope.currentPage = 0;
+    $scope.itemsPerPage = 50;
+    $scope.searchTest   = ''
+
+        $scope.firstPage = function() {
+            return $scope.currentPage == 0;
+        }
+        $scope.lastPage = function() {
+            var lastPageNum = Math.ceil(tableData.items.length / $scope.itemsPerPage - 1);
+            return $scope.currentPage == lastPageNum;
+        }
+        $scope.numberOfPages = function(){
+            return Math.ceil(tableData.items.length / $scope.itemsPerPage);
+        }
+        $scope.startingItem = function() {
+        return $scope.currentPage * $scope.itemsPerPage;
+        }
+        $scope.pageBack = function() {
+            $scope.currentPage = $scope.currentPage - 1;
+        }
+        $scope.pageForward = function() {
+            $scope.currentPage = $scope.currentPage + 1;
+        }
+
+
+        $scope.addItem = function (text, name, price, quantity) {
+
+            price = parseFloat(price);
+            var i = tableData.items.length + 1;
+            if (text != "" && !isNaN(price) && (price && text)) {
+                    // for (var i = 1; ; i < tableData.item.length; i++) {
+                   $scope.list.items.push({id: i, name: text, price: price, quantity: quantity});
+                    // }
+                    i + 1;
+            }
+            
+        }
+
+    });
+
+
+    function blockValue() {
+        elem = document.querySelector('#info_table');
+        elem.style = "display: block";
+            
+        console.log(elem)
+
+    };
+
